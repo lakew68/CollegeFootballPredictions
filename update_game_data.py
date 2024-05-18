2af9df3a7f54fa5e5e67ced9d9949718f827c76d
William Lake <35473988+lakew68@users.noreply.github.com>
2024-05-17 17:25:38 -0700

    Update update_game_data.py
    Adds data reader for weeks that are not yet completed

--- update_game_data.py
+++ update_game_data.py
@@ -96,6 +96,79 @@ def gather_game_data(configuration):
         pickle.dump(cached_games,f)
         
     return cached_games
+
+def gather_new_game_data(configuration):
+    '''Takes in the configuration for the cfb data api and returns statistics organized by college football game for use
+    in predictions.'''
+
+    if os.path.isfile('CFBGameData.dat'):
+        with open("CFBGameData.dat",'rb') as f:
+            cached_games = pickle.load(f) # Multi-level dictionary. 
+                                        #Outer level is year of game, then week, then game id keying a dictionary of game data
+    else:
+        cached_games = {2013:{0:{}}}
+
+    print(cached_games.keys())
+    current_year = datetime.now().year
+    max_year_in_cache = max(cached_games.keys())
+    max_week_in_cache = max(cached_games[max_year_in_cache].keys())
+
+    lines = []
+    games = []
+    year = max(max_year_in_cache, current_year)
+
+    if year == max_year_in_cache:
+        week = max_week_in_cache + 1
+    else:
+        week = 1
+        
+    print('Gathering games')
+    response = games_api.get_games(year=year, week=week)
+    games = [*games, *response]
+
+    response = betting_api.get_lines(year=year, week=week)
+    lines = [*lines, *response]
+
+    games2 = [
+        dict(
+            gid = g.id,
+            year = g.season,
+            week = g.week,
+            neutral_site = g.neutral_site,
+            home_team = g.home_team,
+            home_conference = g.home_conference,
+            home_points = g.home_points,
+            home_elo = g.home_pregame_elo,
+            away_team = g.away_team,
+            away_conference = g.away_conference,
+            away_points = g.away_points,
+            away_elo = g.away_pregame_elo
+        ) for g in games if g.home_points is None and g.away_points is None]
+
+    for game in games2:
+        # This loop finds game betting data for games that have it
+        game_lines = [l for l in lines if l.id == game['gid']]
+        if len(game_lines) > 0:
+            game_line = [l for l in game_lines[0].lines if l.provider == 'consensus']
+            if len(game_line) == 0 or game_line[0].spread is None:
+                if len(game_lines[0].lines) > 0:
+                    game_line = [game_lines[0].lines[0]]
+            if len(game_line) > 0 and game_line[0].spread is not None:
+                game['spread'] = float(game_line[0].spread)
+
+    headers = {'Authorization': configuration.api_key_prefix['Authorization'] + ' ' + configuration.api_key['Authorization']} 
+    games2 = process_games(games2, headers)
+
+    max_year_in_games2 = max([games2[i]['year'] for i in range(len(games2))])
+    
+    stat_keys = ['year', 'week', 'neutral_site', 'home_team', 'home_conference', 'home_points', 'home_elo', 'away_team', 'away_conference', 'away_points', 'away_elo', 'margin', 'spread', ('home', 'rushingYards'), ('home', 'rushingTDs'), ('home', 'passAttempts'), ('home', 'passingTDs'), ('home', 'games'), ('home', 'puntReturnTDs'), ('home', 'firstDowns'), ('home', 'sacks'), ('home', 'interceptionTDs'), ('home', 'kickReturnTDs'), ('home', 'totalYards'), ('home', 'fourthDownConversions'),('home', 'rushingAttempts'),('home', 'possessionTime'),('home', 'fourthDowns'),('home', 'tacklesForLoss'),('home', 'puntReturnYards'),('home', 'passCompletions'),('home', 'puntReturns'),('home', 'kickReturns'),('home', 'thirdDownConversions'),('home', 'fumblesRecovered'),('home', 'passesIntercepted'),('home', 'thirdDowns'),('home', 'kickReturnYards'),('home', 'interceptions'),('home', 'turnovers'),('home', 'penaltyYards'),('home', 'fumblesLost'),('home', 'netPassingYards'),('home', 'penalties'),('home', 'interceptionYards'),('home', 'defense', 'plays', 'perPlay'),('home', 'defense', 'plays'),('home', 'defense', 'drives', 'perPlay'),('home', 'defense', 'drives'),('home', 'defense', 'ppa'),('home', 'defense', 'totalPPA', 'perPlay'),('home', 'defense', 'totalPPA'),('home', 'defense', 'successRate'),('home', 'defense', 'explosiveness'),('home', 'defense', 'powerSuccess'),('home', 'defense', 'stuffRate'),('home', 'defense', 'lineYards'),('home', 'defense', 'lineYardsTotal', 'perPlay'),('home', 'defense', 'lineYardsTotal'),('home', 'defense', 'secondLevelYards'),('home', 'defense', 'secondLevelYardsTotal', 'perPlay'),('home', 'defense', 'secondLevelYardsTotal'),('home', 'defense', 'openFieldYards'),('home', 'defense', 'openFieldYardsTotal', 'perPlay'),('home', 'defense', 'openFieldYardsTotal'),('home', 'defense', 'totalOpportunies', 'perPlay'),('home', 'defense', 'totalOpportunies'),('home', 'defense', 'pointsPerOpportunity'),('home', 'defense', 'fieldPosition', 'averageStart'),('home', 'defense', 'fieldPosition', 'averagePredictedPoints'),('home', 'defense', 'havoc', 'total'),('home', 'defense', 'havoc', 'frontSeven'),('home', 'defense', 'havoc', 'db'),('home', 'defense', 'standardDowns', 'rate'),('home', 'defense', 'standardDowns', 'ppa'),('home', 'defense', 'standardDowns', 'successRate'),('home', 'defense', 'standardDowns', 'explosiveness'),('home', 'defense', 'passingDowns', 'rate'),('home', 'defense', 'passingDowns', 'ppa'),('home', 'defense', 'passingDowns', 'totalPPA', 'perPlay'),('home', 'defense', 'passingDowns', 'totalPPA'),('home', 'defense', 'passingDowns', 'successRate'),('home', 'defense', 'passingDowns', 'explosiveness'),('home', 'defense', 'rushingPlays', 'rate'),('home', 'defense', 'rushingPlays', 'ppa'),('home', 'defense', 'rushingPlays', 'totalPPA', 'perPlay'),('home', 'defense', 'rushingPlays', 'totalPPA'),('home', 'defense', 'rushingPlays', 'successRate'),('home', 'defense', 'rushingPlays', 'explosiveness'),('home', 'defense', 'passingPlays', 'rate'),('home', 'defense', 'passingPlays', 'ppa'),('home', 'defense', 'passingPlays', 'totalPPA', 'perPlay'),('home', 'defense', 'passingPlays', 'totalPPA'),('home', 'defense', 'passingPlays', 'successRate'),('home', 'defense', 'passingPlays', 'explosiveness'),('home', 'offense', 'plays', 'perPlay'),('home', 'offense', 'plays'),('home', 'offense', 'drives', 'perPlay'),('home', 'offense', 'drives'),('home', 'offense', 'ppa'),('home', 'offense', 'totalPPA', 'perPlay'),('home', 'offense', 'totalPPA'),('home', 'offense', 'successRate'),('home', 'offense', 'explosiveness'),('home', 'offense', 'powerSuccess'),('home', 'offense', 'stuffRate'),('home', 'offense', 'lineYards'),('home', 'offense', 'lineYardsTotal', 'perPlay'),('home', 'offense', 'lineYardsTotal'),('home', 'offense', 'secondLevelYards'),('home', 'offense', 'secondLevelYardsTotal', 'perPlay'),('home', 'offense', 'secondLevelYardsTotal'),('home', 'offense', 'openFieldYards'),('home', 'offense', 'openFieldYardsTotal', 'perPlay'),('home', 'offense', 'openFieldYardsTotal'),('home', 'offense', 'totalOpportunies', 'perPlay'),('home', 'offense', 'totalOpportunies'),('home', 'offense', 'pointsPerOpportunity'),('home', 'offense', 'fieldPosition', 'averageStart'),('home', 'offense', 'fieldPosition', 'averagePredictedPoints'),('home', 'offense', 'havoc', 'total'),('home', 'offense', 'havoc', 'frontSeven'),('home', 'offense', 'havoc', 'db'),('home', 'offense', 'standardDowns', 'rate'),('home', 'offense', 'standardDowns', 'ppa'),('home', 'offense', 'standardDowns', 'successRate'),('home', 'offense', 'standardDowns', 'explosiveness'),('home', 'offense', 'passingDowns', 'rate'),('home', 'offense', 'passingDowns', 'ppa'),('home', 'offense', 'passingDowns', 'successRate'),('home', 'offense', 'passingDowns', 'explosiveness'),('home', 'offense', 'rushingPlays', 'rate'),('home', 'offense', 'rushingPlays', 'ppa'),('home', 'offense', 'rushingPlays', 'totalPPA', 'perPlay'),('home', 'offense', 'rushingPlays', 'totalPPA'),('home', 'offense', 'rushingPlays', 'successRate'),('home', 'offense', 'rushingPlays', 'explosiveness'),('home', 'offense', 'passingPlays', 'rate'),('home', 'offense', 'passingPlays', 'ppa'),('home', 'offense', 'passingPlays', 'totalPPA', 'perPlay'),('home', 'offense', 'passingPlays', 'totalPPA'),('home', 'offense', 'passingPlays', 'successRate'),('home', 'offense', 'passingPlays', 'explosiveness'),('away', 'rushingYards'),('away', 'rushingTDs'),('away', 'passAttempts'),('away', 'passingTDs'),('away', 'games'),('away', 'puntReturnTDs'),('away', 'firstDowns'),('away', 'sacks'),('away', 'interceptionTDs'),('away', 'kickReturnTDs'),('away', 'totalYards'),('away', 'fourthDownConversions'),('away', 'rushingAttempts'),('away', 'possessionTime'),('away', 'fourthDowns'),('away', 'tacklesForLoss'),('away', 'puntReturnYards'),('away', 'passCompletions'),('away', 'puntReturns'),('away', 'kickReturns'),('away', 'thirdDownConversions'),('away', 'fumblesRecovered'),('away', 'passesIntercepted'),('away', 'thirdDowns'),('away', 'kickReturnYards'),('away', 'interceptions'),('away', 'turnovers'),('away', 'penaltyYards'),('away', 'fumblesLost'),('away', 'netPassingYards'),('away', 'penalties'),('away', 'interceptionYards'),('away', 'defense', 'plays', 'perPlay'),('away', 'defense', 'plays'),('away', 'defense', 'drives', 'perPlay'),('away', 'defense', 'drives'),('away', 'defense', 'ppa'),('away', 'defense', 'totalPPA', 'perPlay'),('away', 'defense', 'totalPPA'),('away', 'defense', 'successRate'),('away', 'defense', 'explosiveness'),('away', 'defense', 'powerSuccess'),('away', 'defense', 'stuffRate'),('away', 'defense', 'lineYards'),('away', 'defense', 'lineYardsTotal', 'perPlay'),('away', 'defense', 'lineYardsTotal'),('away', 'defense', 'secondLevelYards'),('away', 'defense', 'secondLevelYardsTotal', 'perPlay'),('away', 'defense', 'secondLevelYardsTotal'),('away', 'defense', 'openFieldYards'),('away', 'defense', 'openFieldYardsTotal', 'perPlay'),('away', 'defense', 'openFieldYardsTotal'),('away', 'defense', 'totalOpportunies', 'perPlay'),('away', 'defense', 'totalOpportunies'),('away', 'defense', 'pointsPerOpportunity'),('away', 'defense', 'fieldPosition', 'averageStart'),('away', 'defense', 'fieldPosition', 'averagePredictedPoints'),('away', 'defense', 'havoc', 'total'),('away', 'defense', 'havoc', 'frontSeven'),('away', 'defense', 'havoc', 'db'),('away', 'defense', 'standardDowns', 'rate'),('away', 'defense', 'standardDowns', 'ppa'),('away', 'defense', 'standardDowns', 'successRate'),('away', 'defense', 'standardDowns', 'explosiveness'),('away', 'defense', 'passingDowns', 'rate'),('away', 'defense', 'passingDowns', 'ppa'),('away', 'defense', 'passingDowns', 'totalPPA', 'perPlay'),('away', 'defense', 'passingDowns', 'totalPPA'),('away', 'defense', 'passingDowns', 'successRate'),('away', 'defense', 'passingDowns', 'explosiveness'),('away', 'defense', 'rushingPlays', 'rate'),('away', 'defense', 'rushingPlays', 'ppa'),('away', 'defense', 'rushingPlays', 'totalPPA', 'perPlay'),('away', 'defense', 'rushingPlays', 'totalPPA'),('away', 'defense', 'rushingPlays', 'successRate'),('away', 'defense', 'rushingPlays', 'explosiveness'),('away', 'defense', 'passingPlays', 'rate'),('away', 'defense', 'passingPlays', 'ppa'),('away', 'defense', 'passingPlays', 'totalPPA', 'perPlay'),('away', 'defense', 'passingPlays', 'totalPPA'),('away', 'defense', 'passingPlays', 'successRate'),('away', 'defense', 'passingPlays', 'explosiveness'),('away', 'offense', 'plays', 'perPlay'),('away', 'offense', 'plays'),('away', 'offense', 'drives', 'perPlay'),('away', 'offense', 'drives'),('away', 'offense', 'ppa'),('away', 'offense', 'totalPPA', 'perPlay'),('away', 'offense', 'totalPPA'),('away', 'offense', 'successRate'),('away', 'offense', 'explosiveness'),('away', 'offense', 'powerSuccess'),('away', 'offense', 'stuffRate'),('away', 'offense', 'lineYards'),('away', 'offense', 'lineYardsTotal', 'perPlay'),('away', 'offense', 'lineYardsTotal'),('away', 'offense', 'secondLevelYards'),('away', 'offense', 'secondLevelYardsTotal', 'perPlay'),('away', 'offense', 'secondLevelYardsTotal'),('away', 'offense', 'openFieldYards'),('away', 'offense', 'openFieldYardsTotal', 'perPlay'),('away', 'offense', 'openFieldYardsTotal'),('away', 'offense', 'totalOpportunies', 'perPlay'),('away', 'offense', 'totalOpportunies'),('away', 'offense', 'pointsPerOpportunity'),('away', 'offense', 'fieldPosition', 'averageStart'),('away', 'offense', 'fieldPosition', 'averagePredictedPoints'),('away', 'offense', 'havoc', 'total'),('away', 'offense', 'havoc', 'frontSeven'),('away', 'offense', 'havoc', 'db'),('away', 'offense', 'standardDowns', 'rate'),('away', 'offense', 'standardDowns', 'ppa'),('away', 'offense', 'standardDowns', 'successRate'),('away', 'offense', 'standardDowns', 'explosiveness'),('away', 'offense', 'passingDowns', 'rate'),('away', 'offense', 'passingDowns', 'ppa'),('away', 'offense', 'passingDowns', 'successRate'),('away', 'offense', 'passingDowns', 'explosiveness'),('away', 'offense', 'rushingPlays', 'rate'),('away', 'offense', 'rushingPlays', 'ppa'),('away', 'offense', 'rushingPlays', 'totalPPA', 'perPlay'),('away', 'offense', 'rushingPlays', 'totalPPA'),('away', 'offense', 'rushingPlays', 'successRate'),('away', 'offense', 'rushingPlays', 'explosiveness'),('away', 'offense', 'passingPlays', 'rate'),('away', 'offense', 'passingPlays', 'ppa'),('away', 'offense', 'passingPlays', 'totalPPA', 'perPlay'),('away', 'offense', 'passingPlays', 'totalPPA'),('away', 'offense', 'passingPlays', 'successRate'),('away', 'offense', 'passingPlays', 'explosiveness')]
+    
+    print('Adding games to output dictionary.')
+    new_games = []
+    for game in games2:
+        new_games.append({key:game[key] for key in game if key!='gid'}) # Don't need ID data
+        
+    return new_games
     
 def process_games(games, headers):
     '''Takes in a list of games, where each game is a dictionary of game information. Populates that list with game stats. '''
